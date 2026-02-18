04da9d9ccf03ac46f150b36c0585c3d62d3321fe
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-02-18 21:55:57 +0530

    boiler setup

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+        <h1 className="text-5xl ">About Us page</h1>
+        <p className="text-lg mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis accusamus et quos esse debitis nihil?</p>
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <main className="container mx-auto px-4 py-40 text-center">
+
         {children}
+        </main>
       </body>
     </html>
   );
